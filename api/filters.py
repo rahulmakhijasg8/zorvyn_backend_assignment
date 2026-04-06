79f2d34683aadea476531584241a174e79b78f8e
Rahul <rahulmakhijals7@gmail.com>
2026-04-06 14:33:43 +0530

    filters date

--- api/filters.py
+++ api/filters.py
@@ -1,10 +1,11 @@
 import django_filters
 from .models import Transaction
+from django import forms
 
 
 class TransactionFilter(django_filters.FilterSet):
-    start_date = django_filters.DateTimeFilter(field_name='date', lookup_expr='gte')
-    end_date = django_filters.DateTimeFilter(field_name='date', lookup_expr='lte')
+    start_date = django_filters.DateTimeFilter(widget=forms.DateTimeInput(attrs={'type': 'date'}), field_name='date', lookup_expr='gte')
+    end_date = django_filters.DateTimeFilter(widget=forms.DateTimeInput(attrs={'type': 'date'}), field_name='date', lookup_expr='lte')
 
     class Meta:
         model = Transaction
